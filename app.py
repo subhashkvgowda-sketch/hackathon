858d4f794c9d704d12ebd2d91afb1f32fe47d8b4
subhashkvgowda-sketch <subhashkvgowda@gmail.com>
2026-05-09 14:16:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,165 +1,44 @@
-import streamlit as st
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
 import zipfile
+import hashlib
+def get_file_hash(file_path):
 
-st.set_page_config(
-    page_title="Smart File Organizer Pro",
-    layout="wide"
-)
+    hasher = hashlib.md5()
 
-st.title("📂 Smart File Organizer Pro")
+    with open(file_path, "rb") as f:
 
-uploaded_files = st.file_uploader(
-    "Upload Files",
-    accept_multiple_files=True
-)
+        buffer = f.read()
 
-image_ext = [".jpg", ".png", ".jpeg"]
-doc_ext = [".pdf", ".txt", ".docx"]
-video_ext = [".mp4", ".mkv"]
-music_ext = [".mp3", ".wav"]
-code_ext = [".py", ".html", ".cpp"]
+        hasher.update(buffer)
 
-organized_files = {
-    "Images": [],
-    "Documents": [],
-    "Videos": [],
-    "Music": [],
-    "Code": [],
-    "Others": []
-}
+    return hasher.hexdigest()
+    t.subheader("🛑 Duplicate File Detection")
 
-if st.button("Organize Files"):
+        hashes = {}
 
-    if uploaded_files:
-
-        folders = list(organized_files.keys())
-
-        for folder in folders:
-            os.makedirs(folder, exist_ok=True)
-
-        file_data = []
-
-        for uploaded_file in uploaded_files:
-
-            file_name = uploaded_file.name
-
-            ext = os.path.splitext(file_name)[1].lower()
-
-            if ext in image_ext:
-                category = "Images"
-
-            elif ext in doc_ext:
-                category = "Documents"
-
-            elif ext in video_ext:
-                category = "Videos"
-
-            elif ext in music_ext:
-                category = "Music"
-
-            elif ext in code_ext:
-                category = "Code"
-
-            else:
-                category = "Others"
-
-            save_path = os.path.join(category, file_name)
-
-            with open(save_path, "wb") as f:
-                f.write(uploaded_file.getbuffer())
-
-            organized_files[category].append(save_path)
-
-            size = round(os.path.getsize(save_path) / 1024, 2)
-
-            file_data.append([file_name, category, size])
-
-        st.success("✅ Files Organized Successfully!")
-
-        df = pd.DataFrame(
-            file_data,
-            columns=["File Name", "Category", "Size (KB)"]
-        )
-
-        st.subheader("📋 Organized Files")
-        st.dataframe(df)
-
-        # Search Feature
-        search = st.text_input("🔍 Search File")
-
-        if search:
-
-            filtered_df = df[
-                df["File Name"].str.contains(search, case=False)
-            ]
-
-            st.dataframe(filtered_df)
-
-        # Pie Chart
-        st.subheader("📊 File Distribution")
-
-        category_count = df["Category"].value_counts()
-
-        fig, ax = plt.subplots()
-
-        ax.pie(
-            category_count,
-            labels=category_count.index,
-            autopct='%1.1f%%'
-        )
-
-        st.pyplot(fig)
-
-        # Segregated Files
-        st.subheader("📁 Segregated Files")
+        duplicates = []
 
         for category, files in organized_files.items():
 
-            if files:
+            for file_path in files:
 
-                st.markdown(f"## {category}")
+                file_hash = get_file_hash(file_path)
 
-                for file_path in files:
+                if file_hash in hashes:
 
-                    file_name = os.path.basename(file_path)
+                    duplicates.append(file_path)
 
-                    st.write(f"📄 {file_name}")
+                else:
 
-                    if category == "Images":
-                        st.image(file_path, width=200)
+                    hashes[file_hash] = file_path
 
-                    with open(file_path, "rb") as f:
+        if duplicates:
 
-                        st.download_button(
-                            label=f"⬇ Download {file_name}",
-                            data=f,
-                            file_name=file_name
-                        )
+            st.error("Duplicate Files Found!")
 
-        # CREATE ZIP FILE
-        zip_filename = "Organized_Files.zip"
+            for dup in duplicates:
 
-        with zipfile.ZipFile(zip_filename, "w") as zipf:
+                st.write("📄", os.path.basename(dup))
 
-            for category, files in organized_files.items():
+        else:
 
-                for file_path in files:
-
-                    zipf.write(file_path)
-
-        # DOWNLOAD ZIP
-        with open(zip_filename, "rb") as f:
-
-            st.download_button(
-                label="📦 Download Complete ZIP Folder",
-                data=f,
-                file_name=zip_filename,
-                mime="application/zip"
-            )
-
-    else:
-
-        st.warning("Please upload files")
+            st.success("✅ No Duplicate Files Found")
